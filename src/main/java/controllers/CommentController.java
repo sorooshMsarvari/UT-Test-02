8284ef98fa9963ed83754751576ed42b55d765be
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2023-10-31 21:46:00 +0330

    update project

--- src/main/java/controllers/CommentController.java
+++ src/main/java/controllers/CommentController.java
@@ -1,24 +1,30 @@
 package controllers;
 
-import service.Baloot;
-import model.Comment;
 import exceptions.NotExistentComment;
+import model.Comment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import service.Baloot;
 
 import java.util.Map;
 
 @RestController
 public class CommentController {
+
+    private Baloot baloot = Baloot.getInstance();
+
+    public void setBaloot(Baloot baloot) {
+        this.baloot = baloot;
+    }
     @PostMapping(value = "/comment/{id}/like")
     public ResponseEntity<String> likeComment(@PathVariable String id, @RequestBody Map<String, String> input) {
         int commentId = Integer.parseInt(id);
         try {
-            Comment comment = Baloot.getInstance().getCommentById(commentId);
+            Comment comment = baloot.getCommentById(commentId);
             String username = input.get("username");
             comment.addUserVote(username, "like");
             return new ResponseEntity<>("The comment was successfully liked!", HttpStatus.OK);
@@ -31,7 +37,7 @@ public class CommentController {
     public ResponseEntity<String> dislikeComment(@PathVariable String id, @RequestBody Map<String, String> input) {
         int commentId = Integer.parseInt(id);
         try {
-            Comment comment = Baloot.getInstance().getCommentById(commentId);
+            Comment comment = baloot.getCommentById(commentId);
             String username = input.get("username");
             comment.addUserVote(username, "dislike");
             return new ResponseEntity<>("The comment was successfully disliked!", HttpStatus.OK);
